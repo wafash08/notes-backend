a2a2595e241b2e9fd51ab9fbbc1bb665697e9946
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-11-15 09:27:49 +0700

    part 4 test env, supertest, and initialized the database before tests

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config.js");
+const logger = require("./utils/logger.js");
 const notesRouter = require("./controllers/note");
 const middleware = require("./utils/middleware.js");
-const logger = require("./utils/logger");
+
+mongoose.set("strictQuery", false);
 
 logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch(error => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
 
 app.use(cors());
 app.use(express.static("dist"));
--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const mongoose = require("mongoose");
 const app = require("./app"); // the actual Express application
 const config = require("./utils/config");
-const logger = require("./utils/logger");
 
-mongoose.set("strictQuery", false);
+// mongoose.set("strictQuery", false);
 
-async function connectDB() {
-  try {
-    const conn = await mongoose.connect(config.MONGODB_URI);
-    logger.info(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    logger.error("error connecting to MongoDB:", error.message);
-    process.exit(1);
-  }
-}
+// async function connectDB() {
+//   try {
+//     logger.info("connecting to", config.MONGODB_URI);
+//     const conn = await mongoose.connect(config.MONGODB_URI);
+//     logger.info(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     logger.error("error connecting to MongoDB:", error.message);
+//     process.exit(1);
+//   }
+// }
 
-connectDB().then(() => {
-  app.listen(config.PORT, () => {
-    console.log(`Server is running on port ${config.PORT}`);
-  });
+app.listen(config.PORT, () => {
+  console.log(`Server is running on port ${config.PORT}`);
 });
